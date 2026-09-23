0d729d75d67382000755b3d7f7bc1f36147f53e5
guifarias08 <guilhermepinhofariasdeaquino@gmail.com>
2026-09-23 13:24:44 -0300

    protege acervo com login e adiciona logout

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('books.index');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])
+        ->name('login');
+
+    Route::post('/login', [AuthController::class, 'login'])
+        ->name('login.attempt');
 });
 
-Route::patch(
-    '/books/{book}/rating',
-    [BookController::class, 'rating']
-)->name('books.rating');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
 
-Route::patch(
-    '/books/{book}/favorite',
-    [BookController::class, 'favorite']
-)->name('books.favorite');
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('books.index');
+    });
 
-Route::resource('books', BookController::class);
+    Route::patch(
+        '/books/{book}/rating',
+        [BookController::class, 'rating']
+    )->name('books.rating');
+
+    Route::patch(
+        '/books/{book}/favorite',
+        [BookController::class, 'favorite']
+    )->name('books.favorite');
+
+    Route::resource('books', BookController::class);
+});
